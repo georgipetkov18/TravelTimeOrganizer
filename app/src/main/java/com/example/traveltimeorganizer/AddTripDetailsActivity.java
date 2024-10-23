69dd890608c977bda0be2d9ddc715c480b5325b2
Georgi Petkov <george18petkov@gmail.com>
2024-10-23 19:19:05 +0300

    Add new screens

--- app/src/main/java/com/example/traveltimeorganizer/AddTripDetailsActivity.java
+++ app/src/main/java/com/example/traveltimeorganizer/AddTripDetailsActivity.java
@@ -1,6 +1,5 @@
 package com.example.traveltimeorganizer;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class AddTripDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_trip_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // TODO Implement database and check for upcoming trips
-        if (true) {
-            Intent i = new Intent(this, NoInfoHomeActivity.class);
-            startActivity(i);
-            this.finish();
-        }
-        else {
-
-        }
     }
 }
--- app/src/main/java/com/example/traveltimeorganizer/NoInfoHomeActivity.java
+++ app/src/main/java/com/example/traveltimeorganizer/NoInfoHomeActivity.java
@@ -2,6 +2,7 @@ package com.example.traveltimeorganizer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,27 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class NoInfoHomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_no_info_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // TODO Implement database and check for upcoming trips
-        if (true) {
-            Intent i = new Intent(this, NoInfoHomeActivity.class);
+        ImageButton addTripBtn = findViewById(R.id.addTripBtn);
+        addTripBtn.setOnClickListener(view -> {
+            Intent i = new Intent(this, AddTripDetailsActivity.class);
             startActivity(i);
-            this.finish();
-        }
-        else {
-
-        }
+        });
     }
 }
