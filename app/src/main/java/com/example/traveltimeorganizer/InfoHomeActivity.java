4aec517f9041cbb0ebce1ed5f2f9bc7b3f662cd8
Georgi Petkov <george18petkov@gmail.com>
2024-11-15 23:12:44 +0200

    Create toolbar on info home screen

--- app/src/main/java/com/example/traveltimeorganizer/InfoHomeActivity.java
+++ app/src/main/java/com/example/traveltimeorganizer/InfoHomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.traveltimeorganizer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,6 +19,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.traveltimeorganizer.data.models.Trip;
 import com.example.traveltimeorganizer.utils.Constants;
 import com.example.traveltimeorganizer.utils.TripAdapter;
+import com.google.android.material.appbar.MaterialToolbar;
 
 import java.util.ArrayList;
 
@@ -31,9 +36,27 @@ public class InfoHomeActivity extends AppCompatActivity {
             return insets;
         });
 
-        ArrayList<Trip> trips = (ArrayList<Trip>)getIntent().getExtras().getSerializable(Constants.TABLE_TRIPS_NAME);
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+
+        ArrayList<Trip> trips = (ArrayList<Trip>) getIntent().getExtras().getSerializable(Constants.TABLE_TRIPS_NAME);
         RecyclerView recycler = findViewById(R.id.trip_recycler);
         recycler.setLayoutManager(new LinearLayoutManager((this)));
         recycler.setAdapter(new TripAdapter(trips, this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_toolbar_menu, menu);
+        menu.getItem(0).setOnMenuItemClickListener(menuItem -> {
+            Intent i = new Intent(this, AddTripDetailsActivity.class);
+            startActivity(i);
+            return true;
+        });
+        return true;
     }
 }
